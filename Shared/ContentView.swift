7477354b801b99ff1272e1d22a2676f09bd0c83f
Elmo <elmo5676@gmail.com>
2020-12-28 22:47:56 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+// ********************** ContentView *********************************
+// * Copyright © Cenith Innovations, LLC - All Rights Reserved
+// * Created on 12/28/20, for Shared
+// * Elmo <matt@cenithinnovations.com>
+// * Unauthorized copying of this file is strictly prohibited
+// ********************** ContentView *********************************
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
